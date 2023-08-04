47f66677e929bf9bad9361782af152506e19891a
HarveyHuang <harvey.huang@staff.nueip.com>
2023-08-04 18:18:26 +0800

    stub & fixture

--- cypress/e2e/nueip-testing/attendance.cy.js
+++ cypress/e2e/nueip-testing/attendance.cy.js
@@ -1,12 +1,34 @@
-describe('attendance', () => {
-  context('test', () => {
+describe('attendance record testing - Admin', () => {
+  context('出勤紀錄測試案例', () => {
       beforeEach(() => {
       cy.viewport(1280, 800)
-      cy.loginWithUI({company: 'HHT', username: 'admin', password: 'test12345678'})
+      cy.fixture('users').then((users) => {
+        cy.loginWithUI(users.admin)
+      })
     })
 
-    it('test1', () => {
-      cy.visit('/home')
+    it('新增後修改再刪除', () => {
+      cy.visit('/attendance_record')
+
+      cy.location().then((location) => {
+        expect(location.pathname).to.be.eq('/attendance_record')
+      })
+
+      // cy.intercept(
+      //   {
+      //     method: "POST",
+      //     path: "/shared/org_cascade_select_ajax"
+      //   }
+      // ).as('recordAdd')
+
+      // cy.get('#sys_add')
+      // .click()
+
+      // cy.wait('@recordAdd').then(({request, response}) => {
+      //   cy.get('[data-id="SLayer2"]')
+      //   .should('be.visible')
+      //   .click()
+      // })
     })
   })
 })
--- cypress/e2e/nueip-testing/clock.cy.js
+++ cypress/e2e/nueip-testing/clock.cy.js
@@ -1,41 +1,29 @@
-describe('Clockin & out', () => {
+describe('Clockin & out testing - Admin', () => {
   context('Login' ,() => {
     beforeEach(() => {
       cy.viewport(1280, 800)
-      cy.loginWithUI({company: 'HHT', username: 'admin', password: 'test12345678'})
-    })
-
-    it('successfully clockin', () => {
-      cy.visit('/home')
-
-      // 設定攔截器
-      cy.intercept({
-        method: 'POST',
-        path: '/time_clocks/ajax'
-      }).as('clockin')
-
-      // 點擊上班打卡
-      cy.get('.desktopsite_right')
-      .find('.clock_btn_block')
-      .should('be.visible')
-      .find('#clockin')
-      .click()
-
-      // 等待別名路由
-      cy.wait('@clockin').then(({request, response}) => {
-        expect(response.body.status).to.eq('success')
-        expect(response.body.message).to.eq('打卡成功')
+      cy.fixture('users').then((users) => {
+        cy.loginWithUI(users.user1)
       })
     })
 
-    it('clockin second time in 30s', () => {
+    it('spy & stub', () => {
       cy.visit('/home')
 
-      // 設定攔截器
-      cy.intercept({
-        method: 'POST',
-        path: '/time_clocks/ajax'
-      }).as('clockin')
+      // 設定攔截器，監聽route並塞入假物件response
+      cy.fixture('clockinResponse').then((response) => {
+        cy.intercept(
+          {
+            method: 'POST',
+            path: '/time_clocks/ajax'
+          },
+          {
+            statusCode: 400,
+            body: response.thirtySecsFail,
+            delay: 2000
+          }).as('clockin')
+      })
+
 
       // 點擊上班打卡
       cy.get('.desktopsite_right')
@@ -46,55 +34,54 @@ describe('Clockin & out', () => {
 
       // 等待別名路由
       cy.wait('@clockin').then(({request, response}) => {
-        console.log(response)
+        expect(response.statusCode).to.eq(400)
         expect(response.body.status).to.eq('fail')
-        expect(response.body.message).to.eq('請稍後再打卡')
+        expect(response.body.message).to.eq('30秒內請勿重複打卡')
       })
     })
 
-    it('successfully clockout', () => {
+    it('成功打上班卡', () => {
       cy.visit('/home')
 
       // 設定攔截器
       cy.intercept({
         method: 'POST',
         path: '/time_clocks/ajax'
-      }).as('clockout')
+      }).as('clockin')
 
       // 點擊上班打卡
       cy.get('.desktopsite_right')
       .find('.clock_btn_block')
       .should('be.visible')
-      .find('#clockout')
+      .find('#clockin')
       .click()
 
       // 等待別名路由
-      cy.wait('@clockout').then(({request, response}) => {
-        console.log(response)
+      cy.wait('@clockin').then(({request, response}) => {
         expect(response.body.status).to.eq('success')
         expect(response.body.message).to.eq('打卡成功')
       })
     })
 
-    it('clockout second time in 30s', () => {
+    it('三秒內重複打上班卡', () => {
       cy.visit('/home')
 
       // 設定攔截器
       cy.intercept({
         method: 'POST',
         path: '/time_clocks/ajax'
-      }).as('clockout')
+      }).as('clockin')
 
       // 點擊上班打卡
       cy.get('.desktopsite_right')
       .find('.clock_btn_block')
       .should('be.visible')
-      .find('#clockout')
+      .find('#clockin')
       .click()
 
       // 等待別名路由
-      cy.wait('@clockout').then(({request, response}) => {
-        console.log(response)
+      cy.wait('@clockin').then(({request, response}) => {
+        cy.screenshot()
         expect(response.body.status).to.eq('fail')
         expect(response.body.message).to.eq('請稍後再打卡')
       })
