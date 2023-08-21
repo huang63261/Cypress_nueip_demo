ed243e3ee0dd82a1bdadf191d892903cb9fad001
HarveyHuang <harvey.huang@staff.nueip.com>
2023-08-21 10:44:15 +0800

    打卡測試腳本-新增request打卡

--- cypress/e2e/nueip-testing/clock.cy.js
+++ cypress/e2e/nueip-testing/clock.cy.js
@@ -2,8 +2,37 @@ describe('Clockin & out testing', () => {
   context('Login' ,() => {
     beforeEach(() => {
         cy.viewport(1280, 800)
-        cy.fixture('nueipUsers').then((users) => {
-        cy.loginByCSRF(users.admin)
+        cy.fixture('users/user01').then((user) => {
+        cy.loginByCSRF(user)
+      })
+    })
+
+    it('request 發出打卡請求', () => {
+      cy.visit('/home')
+
+      cy.get('.desktopsite_right')
+      .find('.clock_blcok')
+      .find('input[name="token"]')
+      .invoke('attr', 'value')
+      .then((value) => {
+        cy.request({
+          method: 'POST',
+          url: '/time_clocks/ajax',
+          form: true,
+          body: {
+            action: 'add',
+            id: '1',
+            attendance_time: '2023-8-17 18:07:25',
+            token: value,
+            lat: '25.0511752',
+            lng: '121.5960723',
+          }
+        }).as('clockinRequest')
+      })
+
+      cy.get('@clockinRequest').should((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.status).to.eq('success')
       })
     })
 
@@ -29,6 +58,7 @@ describe('Clockin & out testing', () => {
       .find('.clock_btn_block')
       .should('be.visible')
       .find('#clockin')
+      .scrollIntoView()
       .click()
 
       // 等待別名路由
@@ -39,28 +69,29 @@ describe('Clockin & out testing', () => {
       })
     })
 
-    it('成功打上班卡', () => {
-      cy.visit('/home')
+    // it('成功打上班卡', () => {
+    //   cy.visit('/home')
 
-      // 設定攔截器
-      cy.intercept({
-        method: 'POST',
-        path: '/time_clocks/ajax'
-      }).as('clockin')
+    //   // 設定攔截器
+    //   cy.intercept({
+    //     method: 'POST',
+    //     path: '/time_clocks/ajax'
+    //   }).as('clockin')
 
-      // 點擊上班打卡
-      cy.get('.desktopsite_right')
-      .find('.clock_btn_block')
-      .should('be.visible')
-      .find('#clockin')
-      .click()
+    //   // 點擊上班打卡
+    //   cy.get('.desktopsite_right')
+    //   .find('.clock_btn_block')
+    //   .should('be.visible')
+    //   .find('#clockin')
+    //   .scrollIntoView()
+    //   .click()
 
-      // 等待別名路由
-      cy.wait('@clockin').then(({request, response}) => {
-        expect(response.body.status).to.eq('success')
-        expect(response.body.message).to.eq('打卡成功')
-      })
-    })
+    //   // 等待別名路由
+    //   cy.wait('@clockin', {timeout: 10000}).then(({request, response}) => {
+    //     expect(response.body.status).to.eq('success')
+    //     expect(response.body.message).to.eq('打卡成功')
+    //   })
+    // })
 
     it('三/三十 秒內重複打上班卡', () => {
       cy.visit('/home')
@@ -76,10 +107,11 @@ describe('Clockin & out testing', () => {
       .find('.clock_btn_block')
       .should('be.visible')
       .find('#clockin')
+      .scrollIntoView()
       .click()
 
       // 等待別名路由
-      cy.wait('@clockin').then(({request, response}) => {
+      cy.wait('@clockin', {timeout: 10000}).then(({request, response}) => {
         expect(response.body.status).to.eq('fail')
       })
     })
